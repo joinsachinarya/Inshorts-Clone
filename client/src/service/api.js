868c6d4967cf94be57ad84111f1cb1b4696b5607
Sachin Arya <67674344+joinsachinarya@users.noreply.github.com>
2025-04-24 05:56:35 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:8000';
+const URL = 'https://inshortsapi.vercel.app/news?category=sports';
 
 
 export const getNews = async (page, size = 5) => {
